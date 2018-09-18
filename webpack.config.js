1d7a3a7996f837afe9641c8d4980bf68f99ae535
ishawakankar <iwakankar@hotmail.com>
2018-09-18 11:46:14 +0530

    fc

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    devtool: "inline-cheap-module-source-map",
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                  loader: 'style-loader', // inject CSS to page
+                }, {
+                  loader: 'css-loader', // translates CSS into CommonJS modules
+                },{
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }
+            ]
+            }
+        ]
+    },
+    plugin: [HtmlWebpackPlugin]
+};
+
+module.exports = config;
